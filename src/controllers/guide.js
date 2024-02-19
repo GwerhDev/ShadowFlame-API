e821c29d20f555bb6f6091568390d627a6cb3834
GwerhDev <gwerh.dev@gmail.com>
2024-02-19 11:30:09 -0300

    adding chatbot model endpoint for future models

--- src/controllers/guide.js
+++ src/controllers/guide.js
@@ -4,9 +4,9 @@ const { message } = require('../messages');
 const { aiCohere } = require('../integrations/cohere');
 const { decodeToken } = require('../integrations/jwt');
 
-router.get('/model', async(req, res) => {
+router.get('/chatbot-model', async(req, res) => {
   const model = "cohere";
-  return res.status(200).send({ model });
+  return res.status(200).send(model);
 });
 
 router.post('/chatbot', async(req, res) => {
