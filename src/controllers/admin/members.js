36ddd66318df7338a9f649eff5e9a26092a8288f
GwerhDev <gwerh.dev@gmail.com>
2025-08-10 03:24:08 -0400

    updating member model

--- src/controllers/admin/members.js
+++ src/controllers/admin/members.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
 const Member = require('../../models/Member');
+const userSchema = require('../../models/User');
 const { message } = require('../../messages');
+const { decodeToken } = require('../../integrations/jwt');
 
-router.get('/', async (req, res) => {
+const authorizeRoles = (allowedRoles) => async (req, res, next) => {
+  try {
+    const userToken = req.headers.authorization;
+    if (!userToken) {
+      return res.status(401).send({ message: message.admin.permissionDenied });
+    }
+
+    const decodedToken = await decodeToken(userToken);
+    const user = await userSchema.findOne({ _id: decodedToken.data.id });
+
+    if (!user || !allowedRoles.includes(user.role)) {
+      return res.status(403).send({ message: message.admin.permissionDenied });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(500).send({ error: message.user.error });
+  }
+};
+
+router.get('/', authorizeRoles(['admin', 'leader', 'official']), async (req, res) => {
   try {
     const members = await Member.find();
     return res.status(200).send(members);
@@ -11,7 +33,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', authorizeRoles(['admin', 'leader', 'official']), async (req, res) => {
   try {
     const { username, character, resonance, class: memberClass, whatsapp } = req.body;
     const newMember = await Member.create({ username, character, resonance, class: memberClass, whatsapp });
@@ -21,9 +43,10 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.patch('/:id', authorizeRoles(['admin', 'leader', 'official']), async (req, res) => {
   try {
     const { id } = req.params;
+    console.log(id)
     const updatedMember = await Member.findByIdAndUpdate(id, req.body, { new: true });
     if (!updatedMember) {
       return res.status(404).send({ message: message.member.notfound });
@@ -34,7 +57,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', authorizeRoles(['admin', 'leader', 'official']), async (req, res) => {
   try {
     const { id } = req.params;
     const deletedMember = await Member.findByIdAndDelete(id);
