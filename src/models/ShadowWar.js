dea92919a8d1808fe5602c7aeebb652df9afc6f2
GwerhDev <gwerh.dev@gmail.com>
2025-08-10 14:56:31 -0400

    adding clan and shadow war models

--- src/models/ShadowWar.js
+++ src/models/ShadowWar.js
@@ -1,26 +1,24 @@
 const mongoose = require("mongoose");
 
+const matchSchema = new mongoose.Schema({
+  group1: {
+    member: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
+  },
+  group2: {
+    member: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
+  },
+  result: { type: String },
+});
+
 const shadowWarSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   result: { type: String, required: true },
   enemyClan: { type: mongoose.Schema.Types.ObjectId, ref: 'Clan', required: true },
   battle: {
-    exalted: {
-      member: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
-      result: { type: String },
-    },
-    eminent: {
-      member: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
-      result: { type: String },
-    },
-    famed: {
-      member: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
-      result: { type: String },
-    },
-    proud: {
-      member: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
-      result: { type: String },
-    },
+    exalted: [matchSchema],
+    eminent: [matchSchema],
+    famed: [matchSchema],
+    proud: [matchSchema],
   },
   confirmed: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
 });
